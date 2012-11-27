5586919537957b484f9c65549e7338f2e2819c10
Michael P. Jung <michael.jung@terreon.de>
2012-11-27 21:45:25 +0100

    Add created and updated field to Customer

--- shark/customer/models.py
+++ shark/customer/models.py
@@ -6,6 +6,8 @@ from shark.customer.fields import AddressField
 class BaseCustomer(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     address = AddressField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
