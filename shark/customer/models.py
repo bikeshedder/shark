f78592a05650a564b0dbddf4e6c4865418e8722e
Michael P. Jung <michael.jung@terreon.de>
2012-11-27 21:36:51 +0100

    Add AddressField which includes a *_lines property

--- shark/customer/models.py
+++ shark/customer/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from shark.customer.fields import AddressField
+
 
 class BaseCustomer(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
+    address = AddressField()
 
     class Meta:
         abstract = True
