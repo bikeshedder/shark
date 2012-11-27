6c1b06087c07547a63f607efe3be38e67de6860e
Michael P. Jung <michael.jung@terreon.de>
2012-11-27 21:53:20 +0100

    Add i18n and german translation to customer app

--- shark/customer/models.py
+++ shark/customer/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from shark.customer.fields import AddressField
 
 
 class BaseCustomer(models.Model):
-    id = models.CharField(max_length=20, primary_key=True)
-    address = AddressField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    id = models.CharField(_('id'), max_length=20, primary_key=True)
+    address = AddressField(_('address'))
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    updated = models.DateTimeField(_('updated'), auto_now=True)
 
     class Meta:
+        verbose_name = _('customer')
+        verbose_name_plural = _('customers')
         abstract = True
 
 
