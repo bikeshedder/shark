3d8e4a5cea6c7a76ee20767cccda6cd1eb045f00
Michael P. Jung <michael.jung@terreon.de>
2013-01-29 19:00:58 +0100

    Add get_admin_changelist_url function

--- shark/__init__.py
+++ shark/__init__.py
@@ -24,3 +24,11 @@ def get_admin_change_url(obj):
     model_name = meta.module_name
     view_name = 'admin:%s_%s_change' % (app_label, model_name)
     return reverse(view_name, args=(obj.pk,))
+
+
+def get_admin_changelist_url(obj):
+    meta = obj._meta
+    app_label = meta.app_label
+    model_name = meta.module_name
+    view_name = 'admin:%s_%s_changelist' % (app_label, model_name)
+    return reverse(view_name)
