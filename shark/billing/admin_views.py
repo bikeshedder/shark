316e59784e6c9e23759b4c42b7669b742fc7579c
Michael P. Jung <michael.jung@terreon.de>
2013-02-03 19:51:43 +0100

    Translate invoices depending on customer language

--- shark/billing/admin_views.py
+++ shark/billing/admin_views.py
@@ -9,6 +9,7 @@ from django.template.response import TemplateResponse
 from django.utils.formats import date_format
 from django.utils.translation import ugettext
 from django.utils.translation import ungettext
+from django.utils.translation import override as trans_override
 from pyPdf import PdfFileWriter, PdfFileReader
 
 from shark import get_model
@@ -62,31 +63,32 @@ def invoice_pdf(request, pk):
             for term in INVOICE_TERMS
         ])
 
-    if settings.SHARK['INVOICE']['BACKGROUND']:
-        with tempfile.TemporaryFile() as tmp:
-            # Create content in a temporary file
-            template = BriefTemplate(tmp, document)
+    with trans_override(invoice.language):
+        if settings.SHARK['INVOICE']['BACKGROUND']:
+            with tempfile.TemporaryFile() as tmp:
+                # Create content in a temporary file
+                template = BriefTemplate(tmp, document)
+                template.build(document.content)
+                # Combine background with the content
+                writer = PdfFileWriter()
+                content = PdfFileReader(tmp)
+                info_dict = writer._info.getObject()
+                info_dict.update(content.getDocumentInfo())
+                first_bg = PdfFileReader(file(
+                        settings.SHARK['INVOICE']['BACKGROUND']['FIRST_PAGE']))
+                later_bg = PdfFileReader(file(
+                        settings.SHARK['INVOICE']['BACKGROUND']['LATER_PAGE']))
+                bg = [first_bg.getPage(0), later_bg.getPage(0)]
+                for i, page in enumerate(content.pages):
+                    page.mergePage(bg[min(i, 1)])
+                    page.compressContentStreams()
+                    writer.addPage(page)
+                writer.write(response)
+        else:
+            # Render content directly to the HTTP response object if no
+            # background images are configured.
+            template = BriefTemplate(response, document)
             template.build(document.content)
-            # Combine background with the content
-            writer = PdfFileWriter()
-            content = PdfFileReader(tmp)
-            info_dict = writer._info.getObject()
-            info_dict.update(content.getDocumentInfo())
-            first_bg = PdfFileReader(file(
-                    settings.SHARK['INVOICE']['BACKGROUND']['FIRST_PAGE']))
-            later_bg = PdfFileReader(file(
-                    settings.SHARK['INVOICE']['BACKGROUND']['LATER_PAGE']))
-            bg = [first_bg.getPage(0), later_bg.getPage(0)]
-            for i, page in enumerate(content.pages):
-                page.mergePage(bg[min(i, 1)])
-                page.compressContentStreams()
-                writer.addPage(page)
-            writer.write(response)
-    else:
-        # Render content directly to the HTTP response object if no
-        # background images are configured.
-        template = BriefTemplate(response, document)
-        template.build(document.content)
 
     return response
 
