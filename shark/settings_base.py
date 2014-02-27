eac8d08d43c62311b5b2f0413d19070a2c4de31f
Michael P. Jung <michael.jung@terreon.de>
2014-02-27 11:38:14 +0100

    Move 'static' and 'media' into 'htdocs' directory

--- shark/settings_base.py
+++ shark/settings_base.py
@@ -51,7 +51,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '%s/media' % PROJECT_ROOT
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'htdocs', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -62,7 +62,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '%s/static' % PROJECT_ROOT
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'htdocs', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
