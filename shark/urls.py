599076f0efa2426dd9e1657ee7dfc1d0c9145d9b
Michael P. Jung <michael.jung@terreon.de>
2014-02-27 11:38:36 +0100

    Add serving of media files when DEBUG is enabled

--- shark/urls.py
+++ shark/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -12,3 +13,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin_tools/', include('admin_tools.urls')),
 )
+
+if settings.DEBUG:
+    # Serve media files when DEBUG is enabled
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
