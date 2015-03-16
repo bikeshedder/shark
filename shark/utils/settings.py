e3d4c218efc4b3b0783d235505f00e111e2980df
Michael P. Jung <michael.jung@terreon.de>
2015-03-16 15:02:25 +0100

    Add shark_settings utility method

--- shark/utils/settings.py
+++ shark/utils/settings.py
@@ -15,3 +15,18 @@ def get_settings_value(name, default=RAISE_ERROR):
                 raise KeyError('No such setting: settings.SHARK%s' % ''.join('[%r]' % part for part in parts))
             return default
     return d
+
+
+def shark_settings(d, base=None):
+    if not base:
+        from shark import settings_base
+        base = settings_base.SHARK
+    settings = base.copy()
+    for key, value in d.iteritems():
+        if isinstance(value, dict):
+            if not isinstance(base.get(key, {}), dict):
+                raise RuntimeError('Type mismatch of settings and base settings: %r' % key)
+            settings[key] = shark_settings(value, base.get(key, {}))
+        else:
+            settings[key] = value
+    return settings
