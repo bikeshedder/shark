5e1a57715120c7bc60cf81bb3eaad494f5be8de5
Michael P. Jung <michael.jung@terreon.de>
2025-04-02 12:45:36 +0000

    Replace poetry by uv

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
-poetry run python manage.py migrate
-poetry run python manage.py collectstatic --noinput --turbo
-poetry run gunicorn --workers 1 --threads 8 --timeout 120 -b 0.0.0.0:8000 "shark.wsgi"
+uv run python manage.py migrate
+uv run python manage.py collectstatic --noinput --turbo
+uv run gunicorn --workers 1 --threads 8 --timeout 120 -b 0.0.0.0:8000 "shark.wsgi"
